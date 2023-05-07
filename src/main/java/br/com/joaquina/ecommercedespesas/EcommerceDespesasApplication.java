7b3febf809e7e00a6338bf18edfde66c589d0e6b
ElvisBandeira <bandeirasfrancisco@gmail.com>
2023-05-07 00:02:07 -0300

    primeiro commit

---
+++ src/main/java/br/com/joaquina/ecommercedespesas/EcommerceDespesasApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.ecommercedespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDespesasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceDespesasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joaquina/ecommercedespesas/EcommerceDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.ecommercedespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDespesasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
